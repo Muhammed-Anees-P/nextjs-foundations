cb28680cfe22b6ca36a806fa0736336f1b23d483
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-03 11:12:56 +0530

    feat(ui): add lusitana font
    - add lusitana font for improved typography
    - specify weights 400 and 700 for lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
 import { NextFont } from "next/dist/compiled/@next/font";
-import { Inter } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google";
 
 export const inter: NextFont = Inter({ subsets: ["latin"] });
+
+export const lusitana: NextFont = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
